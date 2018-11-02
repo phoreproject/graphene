0827e2428eb7657f24409de365a3fe0c8c59dc2e
Julian Meyer <julianmeyer2000@gmail.com>
2018-11-02 14:46:33 -0700

    Begin working on validator

--- validator/validator.go
+++ validator/validator.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/inconshreveable/log15"
+import (
+	"context"
+	"flag"
+
+	"google.golang.org/grpc"
+
+	"github.com/inconshreveable/log15"
+	"github.com/phoreproject/synapse/pb"
+)
 
 func main() {
 	log15.Info("Starting validator")
+	beaconHost := flag.String("beaconhost", ":11782", "the address to connect to the beacon node")
+	flag.Parse()
+
+	conn, err := grpc.Dial(*beaconHost, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	rpcClient := pb.NewBlockchainRPCClient(conn)
+	slot, err := rpcClient.GetSlotNumber(context.Background(), &pb.Empty{})
+	if err != nil {
+		panic(err)
+	}
+
+	log15.Info("got slot number", "slot", slot.SlotNumber)
 }

b5d8a0331a9ea3d73882d28aee80a87022d08c25
Julian Meyer <julianmeyer2000@gmail.com>
2018-11-13 17:52:53 -0800

    Add more tests and fix crystallized state copy function

--- blockchain/state_test.go
+++ blockchain/state_test.go
@@ -33,7 +33,7 @@ func GenerateNextBlock(b *blockchain.Blockchain) primitives.Block {
 	return primitives.Block{}
 }
 
-func TestStateActiveValidatorChanges(t *testing.T) {
+func TestStateInitialization(t *testing.T) {
 	var randaoSecret [32]byte
 
 	rand.Read(randaoSecret[:])
@@ -85,5 +85,138 @@ func TestStateActiveValidatorChanges(t *testing.T) {
 	if len(s.Crystallized.ShardAndCommitteeForSlots[0]) == 0 {
 		t.Errorf("invalid initial validator entries")
 	}
-	t.Error("why not")
+
+	if len(s.Crystallized.ShardAndCommitteeForSlots) != blockchain.MainNetConfig.CycleLength*2 {
+		t.Errorf("shardandcommitteeforslots array is not big enough (got: %d, expected: %d)", len(s.Crystallized.ShardAndCommitteeForSlots), blockchain.MainNetConfig.CycleLength)
+	}
+}
+
+func TestCrystallizedStateCopy(t *testing.T) {
+	c := blockchain.CrystallizedState{
+		Crosslinks: []primitives.Crosslink{
+			{
+				RecentlyChanged: false,
+				Slot:            0,
+				Hash:            &zeroHash,
+			},
+		},
+		Validators: []primitives.Validator{
+			{
+				WithdrawalShardID: 0,
+			},
+		},
+		ShardAndCommitteeForSlots: [][]primitives.ShardAndCommittee{
+			{
+				{
+					ShardID: 0,
+				},
+			},
+		},
+		DepositsPenalizedInPeriod: []uint64{1},
+	}
+
+	c1 := c.Copy()
+	c1.Crosslinks[0] = primitives.Crosslink{
+		RecentlyChanged: true,
+		Slot:            1,
+		Hash:            &zeroHash,
+	}
+
+	c1.Validators[0] = primitives.Validator{
+		WithdrawalShardID: 1,
+	}
+
+	c1.ShardAndCommitteeForSlots[0][0] = primitives.ShardAndCommittee{
+		ShardID: 1,
+	}
+
+	c1.DepositsPenalizedInPeriod[0] = 2
+
+	if c.Crosslinks[0].RecentlyChanged {
+		t.Errorf("crystallized state was not copied correctly")
+		return
+	}
+
+	if c.Validators[0].WithdrawalShardID == 1 {
+		t.Errorf("crystallized state was not copied correctly")
+		return
+	}
+
+	if c.ShardAndCommitteeForSlots[0][0].ShardID == 1 {
+		t.Errorf("crystallized state was not copied correctly")
+		return
+	}
+
+	if c.DepositsPenalizedInPeriod[0] == 2 {
+		t.Errorf("crystallized state was not copied correctly")
+		return
+	}
+}
+
+func TestShardCommitteeByShardID(t *testing.T) {
+	committees := []primitives.ShardAndCommittee{
+		{
+			ShardID:   0,
+			Committee: []uint32{0, 1, 2, 3},
+		},
+		{
+			ShardID:   1,
+			Committee: []uint32{4, 5, 6, 7},
+		},
+		{
+			ShardID:   2,
+			Committee: []uint32{0, 1, 2, 3},
+		},
+		{
+			ShardID:   3,
+			Committee: []uint32{99, 100, 101, 102, 103},
+		},
+		{
+			ShardID:   4,
+			Committee: []uint32{0, 1, 2, 3},
+		},
+		{
+			ShardID:   5,
+			Committee: []uint32{4, 5, 6, 7},
+		},
+	}
+
+	s, err := blockchain.ShardCommitteeByShardID(3, committees)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s[0] != 99 {
+		t.Fatal("did not find correct shard")
+	}
+
+	_, err = blockchain.ShardCommitteeByShardID(9, committees)
+	if err == nil {
+		t.Fatal("did not error on non-existant shard ID")
+	}
+
+	shardAndCommitteeForSlots := [][]primitives.ShardAndCommittee{committees}
+
+	s, err = blockchain.CommitteeInShardAndSlot(0, 3, shardAndCommitteeForSlots)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s[0] != 99 {
+		t.Fatal("did not find correct shard")
+	}
+
+	cState := blockchain.CrystallizedState{
+		LastStateRecalculation:    64,
+		ShardAndCommitteeForSlots: shardAndCommitteeForSlots,
+	}
+
+	att, err := cState.GetAttesterIndices(&transaction.Attestation{
+		Slot:    64,
+		ShardID: 3,
+	}, &blockchain.MainNetConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if att[0] != 99 {
+		t.Fatal("did not find correct shard")
+	}
 }

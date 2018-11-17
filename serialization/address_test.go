37cfdcd79af813bb637ecb95b3129aeba87150ed
Julian Meyer <julianmeyer2000@gmail.com>
2018-11-17 14:51:50 -0800

    Add negative address encode/decode tests

--- serialization/address_test.go
+++ serialization/address_test.go
@@ -2,21 +2,16 @@ package serialization_test
 
 import (
 	"bytes"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
 	"testing"
 
-	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcutil/base58"
+
+	"github.com/phoreproject/synapse/bls"
 	"github.com/phoreproject/synapse/serialization"
 )
 
 func TestEncodeDecode(t *testing.T) {
-	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pub := btcec.PublicKey(priv.PublicKey)
+	pub := bls.PublicKey{}
 	addr := serialization.NewAddress(&pub)
 	addr2, err := serialization.DecodeAddress(addr.ToString())
 	if err != nil {
@@ -24,5 +19,29 @@ func TestEncodeDecode(t *testing.T) {
 	}
 	if !bytes.Equal(addr2[:], addr[:]) {
 		t.Fatal("deserialized address does not match serialized address")
+	}
+}
+
+func TestDecodeInvalidAddresses(t *testing.T) {
+	validAddr := "PP8g6YkR881QEzYqTzmYoCSHFrwHe6htJV"
+
+	invalidPrefix := base58.CheckEncode([]byte("aaaaaaaaaaaaaaaaaaaa"), serialization.AddressVersion+1)
+
+	_, err := serialization.DecodeAddress(invalidPrefix)
+	if err == nil {
+		t.Fatal("invalid prefix was accepted by decode address")
+	}
+
+	invalidChecksum := validAddr[:32] + "ht"
+
+	_, err = serialization.DecodeAddress(invalidChecksum)
+	if err == nil {
+		t.Fatal("invalid checksum was accepted by decode address")
+	}
+
+	invalidLength := base58.CheckEncode([]byte("aaaaaaaaaa"), serialization.AddressVersion)
+	_, err = serialization.DecodeAddress(invalidLength)
+	if err == nil {
+		t.Fatal("invalid length was accepted by decode address")
 	}
 }

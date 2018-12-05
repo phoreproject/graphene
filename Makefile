7a5f686c3abc11e51de49498bf63289f2ec29bdd
wqking <wqking@outlook.com>
2018-12-05 17:22:35 +0800

    Moved old integration tests to new folder

--- Makefile
+++ Makefile
@@ -2,25 +2,46 @@ export GO111MODULE=on
 BINARY_NAME=synapsevalidator synapsebeacon synapsep2p
 SRC=$(shell find . -name "*.go")
 
+# The stupid "go build" can't append the .exe on Windows when using -o, let's do it manually
+ifeq ($(OS),Windows_NT)
+EXE=.exe
+else
+EXE=.exe
+endif
+
 all: deps $(BINARY_NAME)
+
 install:
 	go install cmd/beacon/synapsebeacon.go
 	go install cmd/validator/synapsevalidator.go
 	go install cmd/validator/synapsep2p.go
+
 $(BINARY_NAME): $(SRC)
 	go build cmd/beacon/synapsebeacon.go
 	go build cmd/validator/synapsevalidator.go
 	go build cmd/p2p/synapsep2p.go
+
 test: unittest integrationtest
+
 unittest:
 	go test -v ./...
+
+# to be abandoned
 integrationtest: synapsep2p
 	go build -o integration_tests test/cmd/integration.go
 	./integration_tests
+
+src_depend: $(SRC)
+
+integrationtests: src_depend
+	go build -o integration$(EXE) integrationtests/cmd/integrationtests.go
+
 clean:
 	go clean
 	rm -f $(BINARY_NAME)
+
 deps:
 	go build -v ./...
+
 upgrade:
 	go get -u
---
+++ integrationtests/synapsebeacon.go
@@ -0,0 +1,120 @@
+package integrationtests
+
+import (
+	"context"
+	"flag"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/phoreproject/synapse/beacon/primitives"
+	"github.com/phoreproject/synapse/bls"
+	"github.com/phoreproject/synapse/pb"
+	"github.com/phoreproject/synapse/rpc"
+	"github.com/phoreproject/synapse/serialization"
+	"google.golang.org/grpc"
+
+	"github.com/phoreproject/synapse/beacon"
+	"github.com/phoreproject/synapse/beacon/db"
+
+	logger "github.com/sirupsen/logrus"
+)
+
+const clientVersion = "0.0.1"
+
+// SynapseBeaconTest implements IntegrationTest
+type SynapseBeaconTest struct {
+}
+
+// Execute implements IntegrationTest
+func (test SynapseBeaconTest) Execute() error {
+	p2pConnect := flag.String("p2pconnect", "127.0.0.1:11783", "host and port for P2P rpc connection")
+	rpcConnect := flag.String("rpclisten", "127.0.0.1:11782", "host and port for RPC server to listen on")
+	flag.Parse()
+
+	logger.WithField("version", clientVersion).Info("initializing client")
+
+	logger.Info("initializing database")
+	database := db.NewInMemoryDB()
+	c := beacon.MainNetConfig
+
+	logger.Info("initializing blockchain")
+
+	validators := []beacon.InitialValidatorEntry{}
+
+	randaoCommitment := chainhash.HashH([]byte("test"))
+
+	for i := 0; i <= c.CycleLength*(c.MinCommitteeSize*2); i++ {
+		validators = append(validators, beacon.InitialValidatorEntry{
+			PubKey:                bls.PublicKey{},
+			ProofOfPossession:     bls.Signature{},
+			WithdrawalShard:       1,
+			WithdrawalCredentials: serialization.Address{},
+			RandaoCommitment:      randaoCommitment,
+		})
+	}
+
+	logger.WithField("numValidators", len(validators)).Info("initializing blockchain with validators")
+
+	blockchain, err := beacon.NewBlockchainWithInitialValidators(database, &c, validators)
+	if err != nil {
+		return err
+	}
+
+	logger.Info("connecting to p2p RPC")
+	conn, err := grpc.Dial(*p2pConnect, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+
+	p2p := pb.NewP2PRPCClient(conn)
+
+	sub, err := p2p.Subscribe(context.Background(), &pb.SubscriptionRequest{Topic: "block"})
+	if err != nil {
+		return err
+	}
+
+	listener, err := p2p.ListenForMessages(context.Background(), sub)
+	if err != nil {
+		return err
+	}
+	go func() {
+		newBlocks := make(chan primitives.Block)
+		go func() {
+			err := blockchain.HandleNewBlocks(newBlocks)
+			if err != nil {
+				panic(err)
+			}
+		}()
+
+		for {
+			msg, err := listener.Recv()
+			if err != nil {
+				panic(err)
+			}
+
+			var blockProto *pb.Block
+
+			err = proto.Unmarshal(msg.Data, blockProto)
+			if err != nil {
+				continue
+			}
+
+			block, err := primitives.BlockFromProto(blockProto)
+			if err != nil {
+				continue
+			}
+
+			newBlocks <- *block
+		}
+	}()
+
+	logger.Info("initializing RPC")
+
+	err = rpc.Serve(*rpcConnect, blockchain)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

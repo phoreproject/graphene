1839c4ec0cee3fd987d5fc308edc8705cea2e6f3
Julian Meyer <julianmeyer2000@gmail.com>
2019-05-17 11:35:48 -0700

    Bump version to v0.2.1

--- cmd/beacon/synapsebeacon.go
+++ cmd/beacon/synapsebeacon.go
@@ -16,7 +16,7 @@ import (
 	logger "github.com/sirupsen/logrus"
 )
 
-const clientVersion = "0.2.0"
+const clientVersion = "0.2.1"
 
 func main() {
 	rpcConnect := flag.String("rpclisten", "127.0.0.1:11782", "host and port for RPC server to listen on")
